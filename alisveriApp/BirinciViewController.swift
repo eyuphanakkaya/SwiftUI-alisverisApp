29703731c8527bbeb5486be843680328d739125d
Eyuphan Akkaya <akkayaeyupp@gmail.com>
2023-07-06 22:25:35 +0300

    Update
    Tasarımda bir kaç düzenleme yapıldı ve resim eklendi

--- alisveriApp/BirinciViewController.swift
+++ alisveriApp/BirinciViewController.swift
@@ -12,51 +12,56 @@ struct BirinciViewController: View {
     @State private var productList =  [Products]()
 
     var body: some View {
+        ZStack {
+            Color.yellow.edgesIgnoringSafeArea(.all)
         NavigationStack {
-                ScrollView{
-                    LazyVGrid(columns: [GridItem(.flexible()),GridItem(.flexible())],spacing: 20) {
-                        ForEach(productList){ urun in
-                            Tasarim(urun: urun,genislik: 170,yukseklik: 150)
-                                .onTapGesture {
-                                    print(urun.name!)
-                                }
-                        }
+            ScrollView{
+                
+                LazyVGrid(columns: [GridItem(.flexible()),GridItem(.flexible())],spacing: 10) {
+                    ForEach(productList){ urun in
+                        Tasarim(urun: urun,genislik: 180,yukseklik: 250)
+                            .onTapGesture {
+                                print(urun.name!)
+                            }
                     }
                 }
+            }
+            
+            .onAppear{
+                var liste = [Products]()
+                let p1 = Products(id: 1,name: "Nike Koşu Ayakkabı", price: 1200,image: "resim1")
+                let p2 = Products(id: 2,name: "Nike Yürüyüş Ayakkabı", price: 1300,image: "resim1")
+                let p3 = Products(id: 3,name: "Adidas Koşu Ayakkabı", price: 900,image: "resim2")
+                let p4 = Products(id: 4,name: "Puma Koşu Ayakkabı", price: 1100,image: "resim3")
+                let p5 = Products(id: 5,name: "Vans Koşu Ayakkabı", price: 1300,image: "resim2")
+                let p6 = Products(id: 6,name: "Vans Koşu Ayakkabı", price: 1300,image: "resim1")
+                let p7 = Products(id: 7,name: "Hummel Koşu Ayakkabı", price: 1500,image: "resim3")
+                let p8 = Products(id: 8,name: "Hummel Koşu Ayakkabı", price: 1500,image: "resim2")
+                let p9 = Products(id: 9,name: "Hummel Koşu Ayakkabı", price: 1500,image: "resim3")
+                let p10 = Products(id: 10,name: "Hummel Koşu Ayakkabı", price: 1500,image: "resim2")
+                let p11 = Products(id: 11,name: "Hummel Koşu Ayakkabı", price: 1500,image: "resim1")
                 
-                .onAppear{
-                    var liste = [Products]()
-                    let p1 = Products(id: 1,name: "Nike Koşu Ayakkabı", price: 1200)
-                    let p2 = Products(id: 2,name: "Nike Yürüyüş Ayakkabı", price: 1300)
-                    let p3 = Products(id: 3,name: "Adidas Koşu Ayakkabı", price: 900)
-                    let p4 = Products(id: 4,name: "Puma Koşu Ayakkabı", price: 1100)
-                    let p5 = Products(id: 5,name: "Vans Koşu Ayakkabı", price: 1300)
-                    let p6 = Products(id: 6,name: "Vans Koşu Ayakkabı", price: 1300)
-                    let p7 = Products(id: 7,name: "Hummel Koşu Ayakkabı", price: 1500)
-                    let p8 = Products(id: 8,name: "Hummel Koşu Ayakkabı", price: 1500)
-                    let p9 = Products(id: 9,name: "Hummel Koşu Ayakkabı", price: 1500)
-                    let p10 = Products(id: 10,name: "Hummel Koşu Ayakkabı", price: 1500)
-                    let p11 = Products(id: 11,name: "Hummel Koşu Ayakkabı", price: 1500)
-                    
-                    
-                    liste.append(p1)
-                    liste.append(p2)
-                    liste.append(p3)
-                    liste.append(p4)
-                    liste.append(p5)
-                    liste.append(p6)
-                    liste.append(p7)
-                    liste.append(p8)
-                    liste.append(p9)
-                    liste.append(p10)
-                    liste.append(p11)
-                    
-                    productList = liste
-                }
+                
+                liste.append(p1)
+                liste.append(p2)
+                liste.append(p3)
+                liste.append(p4)
+                liste.append(p5)
+                liste.append(p6)
+                liste.append(p7)
+                liste.append(p8)
+                liste.append(p9)
+                liste.append(p10)
+                liste.append(p11)
+                
+                productList = liste
+            }
             
         }.padding(10)
         
     }
+    }
+    
     
     struct Tasarim: View {
         var urun = Products()
@@ -64,9 +69,13 @@ struct BirinciViewController: View {
         var yukseklik = 0.0
         var body: some View {
             VStack(spacing: 20) {
+
+                Image(urun.image!)
+                    .resizable()
+                    .frame(width: 100,height: 100)
                 Text(urun.name!)
-                
                 Text("\(urun.price!)")
+                
             }
             .frame(width: genislik,height: yukseklik)
             .background(Color.blue)
--- alisveriApp/Model/Products.swift
+++ alisveriApp/Model/Products.swift
@@ -6,18 +6,21 @@
 //
 
 import Foundation
+import UIKit
 
 struct Products: Identifiable {
     var id: Int?
     var name: String?
     var price: Int?
+    var image: String?
     
     init() {
     }
     
-    init(id: Int, name: String, price: Int) {
+    init(id: Int, name: String, price: Int,image: String) {
         self.id = id
         self.name = name
         self.price = price
+        self.image = image
     }
 }
