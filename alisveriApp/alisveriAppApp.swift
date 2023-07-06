9f1fb9ceff6680aefa414c13d4869162c45e88d9
Eyuphan Akkaya <akkayaeyupp@gmail.com>
2023-07-06 13:39:18 +0300

    Initial Commit

---
+++ alisveriApp/alisveriAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  alisveriAppApp.swift
+//  alisveriApp
+//
+//  Created by Eyüphan Akkaya on 6.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct alisveriAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
